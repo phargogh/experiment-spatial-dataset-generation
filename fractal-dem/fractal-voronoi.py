9b6e23da960609c3821dfcd44c2f51dbab8ba32e
James Douglass <jdouglass@stanford.edu>
2025-07-25 14:34:31 -0700

    Adding pitfilling, D8 streams, etc.

--- fractal-dem/fractal-voronoi.py
+++ fractal-dem/fractal-voronoi.py
@@ -1,13 +1,19 @@
+import logging
 import math
 import random
+import shutil
 
+import matplotlib.pyplot as plt
 import numpy
 import pygeoprocessing
 import pygeoprocessing.kernels
+import pygeoprocessing.routing
 import shapely
 from osgeo import gdal
 from osgeo import ogr
 from osgeo import osr
+
+logging.basicConfig(level=logging.DEBUG)
 
 AVG_ELEVATION = 1000
 SRS = osr.SpatialReference()
@@ -70,7 +76,6 @@ def recurse(polygon, elevation, depth):
     #    attribute_list=[{'fid': 1}, {'fid': 2}, {'fid': 3}],
     #    ogr_geom_type=ogr.wkbUnknown)
 
-    #import pdb; pdb.set_trace()
     for voronoi_geom in voronoi_geometry.geoms:
         recurse(voronoi_geom.intersection(polygon), elevation, depth + 1)
 
@@ -89,3 +94,46 @@ pygeoprocessing.kernels.linear_decay_kernel('kernel.tif', 5)
 pygeoprocessing.convolve_2d(('dem-voronoi.tif', 1), ('kernel.tif', 1),
                             'convolved-voronoi.tif', mask_nodata=False,
                             ignore_nodata_and_edges=True)
+
+shutil.copyfile('convolved-voronoi.tif', 'convolved-voronoi-pow.tif')
+with gdal.Open('convolved-voronoi-pow.tif', gdal.GA_Update) as ds:
+    band = ds.GetRasterBand(1)
+    array = band.ReadAsArray()
+
+    # Raise to a power to raise peaks and deepen valleys
+    array_pow = array ** 5.3
+
+    # Rescale to a reasonable range, say, 0 - 2,000
+    rescaled = (array_pow / (array_pow.max() - array_pow.min())) * 2000
+
+    band.WriteArray(rescaled)
+
+pygeoprocessing.routing.fill_pits(
+    ('convolved-voronoi-pow.tif', 1), 'voronoi-d8-filled.tif')
+pygeoprocessing.routing.flow_dir_d8(
+    ('voronoi-d8-filled.tif', 1), 'voronoi-d8-dir.tif')
+pygeoprocessing.routing.flow_accumulation_d8(
+    ('voronoi-d8-dir.tif', 1), 'voronoi-d8-accum.tif')
+pygeoprocessing.routing.extract_streams_d8(
+    ('voronoi-d8-accum.tif', 1), 50, 'voronoi-d8-streams.tif')
+
+
+x = numpy.linspace(0, width, width)
+y = numpy.linspace(0, height, height)
+X, Y = numpy.meshgrid(x, y)
+Z = rescaled
+
+# Create the 3D plot
+fig = plt.figure(figsize=(20, 6))
+
+# Method 1: Surface plot
+ax1 = fig.add_subplot(121, projection='3d')
+print(X.shape, y.shape, Z.shape)
+surf = ax1.plot_surface(X, Y, Z, cmap='viridis', alpha=0.8)
+ax1.set_xlabel('X')
+ax1.set_ylabel('Y')
+ax1.set_zlabel('Height')
+ax1.set_title('3D Surface Plot')
+fig.colorbar(surf, ax=ax1, shrink=0.5)
+
+plt.show()
